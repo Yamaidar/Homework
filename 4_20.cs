503f260eed00339216c137abe6b1316f36d3f7ab
Айдар Яманаев <iamanaev6@yandex.ru>
2016-09-26 18:26:14 +0400

    Add files via upload

---
+++ 4_20.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            for (int n = 3; n <= 20; n++)
+            {
+                for (int m = n + 1; m <= 21; m++)
+                {
+                    if (2 * m * n + 2 * m * m == 1000)
+                    {
+                        Console.WriteLine(2 * m * n + "   " + (m * m - n * n) + "   " + (m * m + n * n));
+                        Console.WriteLine("a*b*c = " + (2 * m * n * (m * m - n * n) * (m * m + n * n)));
+                    }
+                }
+            }
+        }
+    }
+}
